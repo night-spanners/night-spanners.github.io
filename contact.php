16cd37940a81b12b9c66eaf75302eb6f150cc4bb
johnmoreland <jmoreland@outlook.com>
2016-02-16 09:56:55 -0500

    Modified php script

--- contact.php
+++ contact.php
@@ -1,21 +1,20 @@
 <?php
-$field_name = $_POST['cf_name'];
+// $field_name = $_POST['cf_name'];
 $field_email = $_POST['cf_email'];
-$field_message = $_POST['cf_message'];
+// $field_message = $_POST['cf_message'];
 
 $mail_to = 'night.spanners@gmail.com';
-$subject = 'Message from a site visitor '.$field_name;
+$subject = 'Message from a site visitor '.;
 
 // $body_message = 'From: '.$field_name."\n";
-$body_message .= 'E-mail: '.$email."\n";
+$body_message .= 'E-mail: '.$field_email."\n";
 // $body_message .= 'Message: '.$field_message;
 
-// $headers = 'From: '.$field_email."\r\n";
-// $headers .= 'Reply-To: '.$field_email."\r\n";
-$headers = 'woohoo!'
 
-$mail_status = mail($mail_to, $subject, $body_message, $headers);
+$mail_status = mail($mail_to, $subject, $body_message);
 // $mail_status = mail($mail_to, $subject, $body_message)
+
+
 
 if ($mail_status) { ?>
 	<script language="javascript" type="text/javascript">
